acc7d29cfa134fbc75bfec15ae5e9606326f9df4
Vinnie Lopez <vinnielo01@gmail.com>
2026-04-30 13:34:44 -0700

    fix log in server went down

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 // import Story from "../../../models/storyModel";
+const API_BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://once-upon-an-adventure-api.onrender.com"
+    : "";
 
 export default {
   // Get all users
   getUser: function (id) {
     // return axios.get("https://once-upon-an-adventure-api.onrender.com/api/signup", userData );
-    return fetch(`/api/user/${id}`, {
+    return fetch(`${API_BASE_URL}/api/user/${id}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -17,7 +21,7 @@ export default {
   // Saves a User to the database
   saveUser: function (userData) {
     // return axios.post("https://once-upon-an-adventure-api.onrender.com/api/user", userData);
-    return fetch("/api/user", {
+    return fetch(`${API_BASE_URL}/api/user`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -28,7 +32,7 @@ export default {
 
   findUser: function (userData) {
     // return axios.post("https://once-upon-an-adventure-api.onrender.com/api/user/login", userData);
-    return fetch("/api/user/login", {
+    return fetch(`${API_BASE_URL}/api/user/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -43,47 +47,47 @@ export default {
 
   getSprite: function (spriteData) {
     return axios.get(
-      "/api/sprite",
+      `${API_BASE_URL}/api/sprite`,
       spriteData
     );
   },
 
   getUserSprite: function (id) {
     return axios.get(
-      `/api/user/avatar/${id}`
+      `${API_BASE_URL}/api/user/avatar/${id}`
     );
   },
 
   getUserStory: function (id) {
     return axios.get(
-      `/api/user/avatar/story/${id}`
+      `${API_BASE_URL}/api/user/avatar/story/${id}`
     );
   },
 
   getUserInventory: function (id) {
     return axios.get(
-      `/api/user/inventory/${id}`
+      `${API_BASE_URL}/api/user/inventory/${id}`
     );
   },
 
   UpdateSpriteLives: function (spriteData, id) {
     return axios.put(
-      "/api/sprite/lives/" +
-        id +
-        "/" +
-        spriteData
+      `${API_BASE_URL}/api/sprite/lives/` +
+      id +
+      "/" +
+      spriteData
     );
   },
 
   UpdateSpriteMoney: function (spriteData, id) {
-   return fetch(`/api/sprite/money/${id}/${spriteData}`, {
+    return fetch(`${API_BASE_URL}/api/sprite/money/${id}/${spriteData}`, {
       method: 'PUT',
     })
   },
   UpdateSpriteHomeFirst: function (spriteData, id) {
     return axios.put(
-      "/api/sprite/homefirst/" +
-        id,
+      `${API_BASE_URL}/api/sprite/homefirst/` +
+      id,
       spriteData
     );
     // return fetch(`/api/sprite/homefirst/${id}`,{
@@ -97,24 +101,24 @@ export default {
 
   UpdateSpriteFirstGuardTalk: function (spriteData, id) {
     return axios.put(
-      "/api/sprite/guardTalk/" +
-        id,
+      `${API_BASE_URL}/api/sprite/guardTalk/` +
+      id,
       spriteData
     );
   },
 
   UpdateSpriteFirstOrcTalk: function (spriteData, id) {
     return axios.put(
-      "/api/sprite/orcTalk/" +
-        id,
+      `${API_BASE_URL}/api/sprite/orcTalk/` +
+      id,
       spriteData
     );
   },
 
   UpdateSpriteFirstJaceTalk: function (spriteData, id) {
     return axios.put(
-      "/api/sprite/jaceTalk/" +
-        id,
+      `${API_BASE_URL}/api/sprite/jaceTalk/` +
+      id,
       spriteData
     );
   },
@@ -122,8 +126,8 @@ export default {
   UpdateSpriteFirstThiefTalk: function (spriteData, id) {
     console.log(spriteData);
     return axios.put(
-      "/api/sprite/thiefTalk/" +
-        id,
+      `${API_BASE_URL}/api/sprite/thiefTalk/` +
+      id,
       spriteData
     );
     // return fetch(`/api/sprite/thiefTalk/${id}`, {
@@ -137,32 +141,32 @@ export default {
 
   UpdateSpritePlace: function (spriteData, id) {
     return axios.put(
-      "/api/sprite/place/" +
-        id +
-        "/" +
-        spriteData
+      `${API_BASE_URL}/api/sprite/place/` +
+      id +
+      "/" +
+      spriteData
     );
   },
 
   UpdateSpritePermit: function (spriteData, id) {
     return axios.put(
-      "/api/sprite/permit/" + id,
+      `${API_BASE_URL}/api/sprite/permit/` + id,
       spriteData
     );
   },
 
   UpdateStory: function (storyData, id) {
     return axios.put(
-      "/api/story/update/" +
-        id +
-        "/" +
-        storyData
+      `${API_BASE_URL}/api/story/update/` +
+      id +
+      "/" +
+      storyData
     );
   },
 
   saveSprite: function (spriteData, id) {
     // return axios.post("/api/sprite/" + id, spriteData);
-    return fetch(`/api/sprite/${id}`, {
+    return fetch(`${API_BASE_URL}/api/sprite/${id}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -172,15 +176,15 @@ export default {
   },
 
   createStory: function (storyData, id) {
-    return axios.post("/api/story/" + id, storyData);
+    return axios.post(`${API_BASE_URL}/api/story/` + id, storyData);
   },
 
   getInventory: function (spriteData) {
-    return axios.get("/api/inventory", spriteData);
+    return axios.get(`${API_BASE_URL}/api/inventory`, spriteData);
   },
   saveInventory: function (spriteData, id) {
     return axios.post(
-      "/api/inventory" + id,
+      `${API_BASE_URL}/api/inventory` + id,
       spriteData
     );
   },
--- src/utils/newApi.js
+++ src/utils/newApi.js
@@ -1,10 +1,14 @@
-export const getUserSprite = (id) =>{
-    return fetch(`api/user/avatar/${id}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-  }
+const API_BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://once-upon-an-adventure-api.onrender.com"
+    : "";
 
- 
+export const getUserSprite = (id) => {
+  return fetch(`${API_BASE_URL}/api/user/avatar/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
+
